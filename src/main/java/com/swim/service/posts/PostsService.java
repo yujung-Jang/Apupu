d066760190c97f3d4d455aeb23690bfaf850da7c
yujung-Jang <yjung.dev@gmail.com>
2023-02-09 16:30:01 +0900

    :sparkles: feat: PostsService update 메서드 구현

--- src/main/java/com/swim/service/posts/PostsService.java
+++ src/main/java/com/swim/service/posts/PostsService.java
@@ -3,6 +3,7 @@ package com.swim.service.posts;
 import com.swim.domain.posts.Posts;
 import com.swim.domain.posts.PostsRepository;
 import com.swim.web.dto.PostsSaveRequestDto;
+import com.swim.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,15 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
     }
 
     public void delete(Long id) {
