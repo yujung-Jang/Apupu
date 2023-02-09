de6f409de8fd8f38e0b08d46c84639b42b2dd41a
yujung-Jang <yjung.dev@gmail.com>
2023-02-09 15:29:08 +0900

    :sparkles: feat: PostsService save 메서드 구현

---
+++ src/main/java/com/swim/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.swim.service.posts;
+
+import com.swim.domain.posts.PostsRepository;
+import com.swim.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
