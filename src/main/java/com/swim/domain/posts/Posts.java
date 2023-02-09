14e595428e0cdee9fec817560ad99cbfa8820d0f
yujung-Jang <yjung.dev@gmail.com>
2023-02-09 13:52:07 +0900

    :sparkles: Posts 엔티티 구현

---
+++ src/main/java/com/swim/domain/posts/Posts.java
@@ -0,0 +1,30 @@
+package com.swim.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Builder
+    public Posts(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+}
