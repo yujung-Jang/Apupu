07b8274d1ea62f4875b13c5e41c62f36deb611b1
yujung-Jang <yjung.dev@gmail.com>
2023-02-09 16:37:29 +0900

    :sparkles: feat: PostsService findById 메서드 구현

--- src/main/java/com/swim/service/posts/PostsService.java
+++ src/main/java/com/swim/service/posts/PostsService.java
@@ -2,12 +2,12 @@ package com.swim.service.posts;
 
 import com.swim.domain.posts.Posts;
 import com.swim.domain.posts.PostsRepository;
+import com.swim.web.dto.PostsResponseDto;
 import com.swim.web.dto.PostsSaveRequestDto;
 import com.swim.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -20,6 +20,7 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
@@ -29,10 +30,19 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
     public void delete(Long id) {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
