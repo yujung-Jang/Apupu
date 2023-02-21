5b2384a9ebf5cbadcadc617f06c0343777cb94c3
yujung-Jang <yjung.dev@gmail.com>
2023-02-21 11:27:35 +0900

    :recycle: refactor: @LoginUser 적용

--- src/main/java/com/apupu/web/IndexController.java
+++ src/main/java/com/apupu/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.apupu.web;
 
+import com.apupu.config.auth.LoginUser;
 import com.apupu.config.auth.dto.SessionUser;
 import com.apupu.service.posts.PostsService;
 import com.apupu.web.dto.PostsResponseDto;
@@ -9,19 +10,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) { // 값이 없으면 로그인 버튼이 보임
             model.addAttribute("userName", user.getName());
