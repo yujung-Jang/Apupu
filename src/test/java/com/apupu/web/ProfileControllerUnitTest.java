20bac41aa4d3a69a32140fcb51d5c8015be599af
yujung-Jang <yjung.dev@gmail.com>
2023-03-02 15:22:11 +0900

    :white_check_mark: test: ProfileController의 profile 메서드 테스트

---
+++ src/test/java/com/apupu/web/ProfileControllerUnitTest.java
@@ -0,0 +1,27 @@
+package com.apupu.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile이_조회된다() {
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
