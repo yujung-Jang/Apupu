3b7fded95b15dbd3678aa2a5abb86465704c92f9
yujung-Jang <yjung.dev@gmail.com>
2023-03-02 15:25:31 +0900

    :white_check_mark: test: ProfileController의 profile 메서드 테스트

--- src/test/java/com/apupu/web/ProfileControllerUnitTest.java
+++ src/test/java/com/apupu/web/ProfileControllerUnitTest.java
@@ -1,5 +1,6 @@
 package com.apupu.web;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.env.MockEnvironment;
 
@@ -15,6 +16,39 @@ public class ProfileControllerUnitTest {
         env.addActiveProfile(expectedProfile);
         env.addActiveProfile("oauth");
         env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void real_profile이_없으면_첫번째가_조회된다() {
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void active_profile이_없으면_default가_조회된다() {
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
 
         ProfileController controller = new ProfileController(env);
 
